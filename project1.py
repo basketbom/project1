53d26634e3709e417f0d7e26ef9c44718e8c37d0
basketbom <basketbom@gmail.com>
2019-08-26 16:56:49 +0700

    Update project1.py

--- project1.py
+++ project1.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "<h1>Hello00000000, Flask application.</h1>"
+    return "<h1>Hello000000088, Flask application.</h1>"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
